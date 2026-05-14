7f9739db75086e4f6bc09eb116adf82ee95c4167
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-05-14 17:22:42 +0200

    comprobado uso de la api

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class HomeScreen extends StatelessWidget {
 
@@ -9,7 +10,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Placeholder(),
+      body: Center(
+        child: Text( dotenv.env['THE_MOVIEDB_KEY'] ?? 'No hay api key'),
+      ),
     );
   }
 }

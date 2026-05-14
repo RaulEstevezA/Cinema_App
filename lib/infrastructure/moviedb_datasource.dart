e21623e4e97fb71a8e3360aad3b83eac0b9b43be
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-05-14 17:39:42 +0200

    creado el inicio del archivo para obtener la data

---
+++ lib/infrastructure/moviedb_datasource.dart
@@ -0,0 +1,27 @@
+
+
+import 'package:cinema_app/config/constants/enviroment.dart';
+import 'package:cinema_app/domain/datasources/movies_datasources.dart';
+import 'package:cinema_app/domain/entities/movies.dart';
+import 'package:dio/dio.dart';
+
+class MoviedbDatasource extends MoviesDataSources {
+  
+  final dio = Dio(BaseOptions(
+      baseUrl: 'https://api.themoviedb.org/3',
+      queryParameters: {
+        'api_key': Enviroment.theMovideDbKey,
+        'language': 'es-ES'
+      }
+  ));
+  
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async {
+
+    final response = await dio.get('/movie/now:playing');
+    final List<Movie> movies = [];
+
+    return [];
+  }
+
+}

d5a7f13b332d865d4322376b5de23ad8ee3432cb
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-05-18 14:16:08 +0200

    añadir mapeador para transformar la información

---
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -0,0 +1,25 @@
+import 'package:cinema_app/domain/entities/movies.dart';
+import 'package:cinema_app/infrastructure/models/moviedb/movie_moviedb.dart';
+
+class MovieMapper {
+  static Movie movieDBToEntity(MovieMovieDB moviedb) => Movie(
+    backdropPath: (moviedb.backdropPath != '')
+    ? 'https://image.tmdb.org/t/p/w500${ moviedb.backdropPath }'
+    : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoWcWg0E8pSjBNi0TtiZsqu8uD2PAr_K11DA&s',
+    genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
+    id: moviedb.id,
+    adult: moviedb.adult,
+    originalLanguage: moviedb.originalLanguage,
+    originalTitle: moviedb.originalTitle,
+    overview: moviedb.overview,
+    popularity: moviedb.popularity,
+    posterPath: (moviedb.posterPath != '')
+    ? 'https://image.tmdb.org/t/p/w500${ moviedb.backdropPath }'
+    : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoWcWg0E8pSjBNi0TtiZsqu8uD2PAr_K11DA&s',
+    releaseDate: moviedb.releaseDate,
+    title: moviedb.title,
+    video: moviedb.video,
+    voteAverage: moviedb.voteAverage,
+    voteCount: moviedb.voteCount,
+  );
+}

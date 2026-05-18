6a7a501873ea2a2886ffe3b2bb5b7eaefec4fe87
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-05-18 14:25:14 +0200

    añadido codigo para obtener la informacion y mapearla

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -3,6 +3,8 @@
 import 'package:cinema_app/config/constants/enviroment.dart';
 import 'package:cinema_app/domain/datasources/movies_datasources.dart';
 import 'package:cinema_app/domain/entities/movies.dart';
+import 'package:cinema_app/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinema_app/infrastructure/models/moviedb/moviedb_response.dart';
 import 'package:dio/dio.dart';
 
 class MoviedbDatasource extends MoviesDataSources {
@@ -19,12 +21,16 @@ class MoviedbDatasource extends MoviesDataSources {
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
 
     final response = await dio.get('/movie/now:playing');
-    response.data;
-    final List<Movie> movies = [];
+    
+    final movieDbResponse = MovieDbResponse.fromJson(response.data);
+
+    final List<Movie> movies = movieDbResponse.results.map(
+      (moviedb) => MovieMapper.movieDBToEntity(moviedb)
+      ).toList();
 
     
 
-    return [];
+    return movies;
   }
 
 }

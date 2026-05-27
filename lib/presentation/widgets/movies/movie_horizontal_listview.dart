4dd2a113595474144822b0f93ab9476dee91b927
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-05-27 14:00:23 +0200

    creado titulo de las peliculas horizontales

--- lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -12,6 +12,50 @@ class MovieHorizontalListview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return SizedBox(
+      height: 350,
+      child: Column(
+        children: [
+
+          if(title != null || subTitle != null)
+            _Title(title: title, subTitle: subTitle)
+
+
+        ],
+      ),
+    );
+  }
+}
+
+class _Title extends StatelessWidget {
+  final String? title;
+  final String? subTitle;
+
+  const _Title({this.title, this.subTitle});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+
+    return Container(
+      padding: const EdgeInsets.only(top: 10),
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      child: Row(
+        children: [
+          if (title != null)
+            Text(title!, style: titleStyle),
+
+          const Spacer(),
+
+          if (subTitle != null)
+            FilledButton.tonal(
+              style: const ButtonStyle( visualDensity : VisualDensity.compact),
+              onPressed: (){}, 
+              child: Text(subTitle!),
+            ) 
+        ],
+      ),
+    );
   }
 }

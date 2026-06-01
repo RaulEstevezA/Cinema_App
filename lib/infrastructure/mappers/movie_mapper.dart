2cb4d99ead891283c9145353731113fbcf72fe0b
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-06-01 14:25:16 +0200

    corregido el enlace de poster en el mapeador de películas para usar la ruta correcta

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -14,7 +14,7 @@ class MovieMapper {
     overview: moviedb.overview,
     popularity: moviedb.popularity,
     posterPath: (moviedb.posterPath != '')
-    ? 'https://image.tmdb.org/t/p/w500${ moviedb.backdropPath }'
+    ? 'https://image.tmdb.org/t/p/w500${ moviedb.posterPath }'
     : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoWcWg0E8pSjBNi0TtiZsqu8uD2PAr_K11DA&s',
     releaseDate: moviedb.releaseDate,
     title: moviedb.title,

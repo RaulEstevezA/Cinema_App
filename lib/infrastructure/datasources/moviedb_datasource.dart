be670be61c8fd1aa262f49a30d5e928dae24bf01
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-06-12 12:58:53 +0200

    agregado el método getTopRated en MoviedbDatasource para obtener películas mejor valoradas

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -63,4 +63,16 @@ class MoviedbDatasource extends MoviesDataSources {
 
     return _jsonToMovies(response.data);
   }
+  
+  @override
+  Future<List<Movie>> getTopRated({int page = 1}) async {
+    final response = await dio.get(
+      '/movie/top_rated',
+      queryParameters: {
+        'page': page,
+      },
+    );
+
+    return _jsonToMovies(response.data);
+  }
 }

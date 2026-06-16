d7e9fede351f8ec3a1978d1dbc0d76176e5b9f30
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-06-16 17:40:14 +0200

    agregado el widget MobieScreen para mostrar la pantalla de detalles de la película

---
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MobieScreen extends StatelessWidget {
+
+  static const name = 'movie-screen';
+
+  final String movieId;
+
+  const MobieScreen({
+    super.key, 
+    required this.movieId
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

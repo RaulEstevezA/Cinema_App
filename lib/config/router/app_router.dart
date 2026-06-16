41aba6398569c7d36d389b319cb575f7802ec947
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-06-16 17:52:59 +0200

    agregado el enrutamiento para la pantalla de detalles de la película en app_router.dart

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -13,5 +13,15 @@ final appRouter = GoRouter(
     
     ),
 
+    GoRoute(
+      path: '/movie/:id',
+      name: MovieScreen.name,
+      builder: (context, state) {
+        final movieId = state.pathParameters['id'] ?? 'no-id';
+        return MovieScreen(movieId : movieId);
+      },
+    
+    ),
+
   ]
 );

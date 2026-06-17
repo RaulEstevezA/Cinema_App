1909ab837c93951c5a88750fe2bb122a8b1747ab
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-06-17 22:36:51 +0200

    agregado el método movieDetailsToEntity en MovieMapper para mapear detalles de películas a entidades Movie

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -1,4 +1,5 @@
 import 'package:cinema_app/domain/entities/movies.dart';
+import 'package:cinema_app/infrastructure/models/moviedb/movie_details.dart';
 import 'package:cinema_app/infrastructure/models/moviedb/movie_moviedb.dart';
 
 class MovieMapper {
@@ -22,4 +23,28 @@ class MovieMapper {
     voteAverage: double.parse(moviedb.voteAverage.toStringAsFixed(1)),
     voteCount: moviedb.voteCount,
   );
+
+
+  static Movie movieDetailsToEntity (MovieDetails moviedb) => Movie(
+    backdropPath: (moviedb.backdropPath != '')
+    ? 'https://image.tmdb.org/t/p/w500${ moviedb.backdropPath }'
+    : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoWcWg0E8pSjBNi0TtiZsqu8uD2PAr_K11DA&s',
+    genreIds: moviedb.genres.map((e) => e.name).toList(),
+    id: moviedb.id,
+    adult: moviedb.adult,
+    originalLanguage: moviedb.originalLanguage,
+    originalTitle: moviedb.originalTitle,
+    overview: moviedb.overview,
+    popularity: moviedb.popularity,
+    posterPath: (moviedb.posterPath != '')
+    ? 'https://image.tmdb.org/t/p/w500${ moviedb.posterPath }'
+    : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoWcWg0E8pSjBNi0TtiZsqu8uD2PAr_K11DA&s',
+    releaseDate: moviedb.releaseDate,
+    title: moviedb.title,
+    video: moviedb.video,
+    voteAverage: double.parse(moviedb.voteAverage.toStringAsFixed(1)),
+    voteCount: moviedb.voteCount,
+  );
 }
+
+

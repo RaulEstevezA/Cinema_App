995117f29b0e103886ba6067a2d5f67a3719b76f
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-06-18 01:13:58 +0200

    agregado ActorsMoviedbDatasource para obtener actores por película desde la API de The Movie Database

---
+++ lib/infrastructure/datasources/actors_moviedb_datasource.dart
@@ -0,0 +1,32 @@
+import 'package:cinema_app/config/constants/enviroment.dart';
+import 'package:cinema_app/domain/entities/actor.dart';
+import 'package:cinema_app/domain/repositories/actores_repositories.dart';
+import 'package:cinema_app/infrastructure/mappers/actor_mapper.dart';
+import 'package:cinema_app/infrastructure/models/moviedb/credits_response.dart';
+import 'package:dio/dio.dart';
+
+class ActorsMoviedbDatasource extends ActorsRepository{
+  
+  final dio = Dio(BaseOptions(
+    baseUrl: 'https://api.themoviedb.org/3',
+    queryParameters: {
+      'api_key': Enviroment.theMovideDbKey,
+      'language': 'es-ES',
+    },
+  ));
+  
+  
+  
+  @override
+  Future<List<Actor>> getActorsByMovie(String movieId) async {
+    final response = await dio.get(
+      '/movie/$movieId/credits',
+    );
+
+    final castResponse = CreditsResponse.fromJson(response.data);
+
+    List<Actor> actors= castResponse.cast.map((cast) => ActorMapper.castToEntity(cast)).toList();
+
+    return actors;
+  }
+}

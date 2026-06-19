2103c24048f8f52cc176efd594e630ff8bbbfef4
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-06-19 13:38:26 +0200

    corregido parámetro de búsqueda en el método searchMovies en MoviesDataSources

--- lib/domain/datasources/movies_datasources.dart
+++ lib/domain/datasources/movies_datasources.dart
@@ -12,5 +12,5 @@ abstract class MoviesDataSources {
 
   Future<Movie>getMovieById(String id);
 
-  Future<List<Movie>> searchMovies ({String query});
+  Future<List<Movie>> searchMovies (String query);
 }

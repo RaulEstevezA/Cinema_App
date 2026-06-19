83756f9cb4ec0c314e6f4cd980a14c66701a51c2
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-06-19 15:30:37 +0200

    mejorada la clase _MovieItem para ajustar el radio de borde y mostrar la calificación de la película

--- lib/presentation/delagate/search_movie_delegate.dart
+++ lib/presentation/delagate/search_movie_delegate.dart
@@ -1,4 +1,5 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:cinema_app/config/helpers/human_formats.dart';
 import 'package:cinema_app/domain/entities/movies.dart' show Movie;
 import 'package:flutter/material.dart';
 
@@ -85,7 +86,7 @@ class _MovieItem extends StatelessWidget {
           //Image
           SizedBox(width: size.width * 0.2,
           child: ClipRRect(
-            borderRadius: BorderRadius.circular(20),
+            borderRadius: BorderRadius.circular(10),
             child: Image.network(
               movie.posterPath,
               loadingBuilder: (context, child, loadingProgress) => FadeIn(child: child),
@@ -107,7 +108,16 @@ class _MovieItem extends StatelessWidget {
                   ? Text('${movie.overview.substring(0,100)}...')
                   : Text(movie.overview),
 
-                
+                Row(
+                  children: [
+                    Icon(Icons.star_half_rounded, color: Colors.yellow.shade800,),
+                    const SizedBox(width: 5),
+                    Text(
+                      HumanFormats.number(movie.voteAverage, 1),
+                      style: TextStyle.bodyMedium!.copyWith(color: Colors.yellow.shade900),
+                    ),
+                  ],
+                ),
               ],
             ),
           )

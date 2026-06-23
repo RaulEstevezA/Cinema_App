8dbc0041f4d6a52ae53f156de8a0a829eb8e7de2
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-06-23 13:33:59 +0200

    refactor: simplificar HomeScreen eliminando _HomeView y utilizando childView

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,115 +1,22 @@
-import 'package:cinema_app/presentation/providers/providers.dart';
 import 'package:cinema_app/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:intl/intl.dart';
+
 
 class HomeScreen extends StatelessWidget {
 
   static const name = 'home-screen';
 
-  const HomeScreen({super.key});
+  final Widget childView;
+
+  const HomeScreen({super.key, required this.childView});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _HomeView(),
-      bottomNavigationBar: CustomBottomNavigation(),
+      body: childView,
+      bottomNavigationBar: const CustomBottomNavigation(),
       
     );
   }
 }
 
-class _HomeView extends ConsumerStatefulWidget {
-  const _HomeView();
-
-  @override
-  _HomeViewState createState() => _HomeViewState();
-}
-
-class _HomeViewState extends ConsumerState<_HomeView> {
-
-  @override
-  void initState() {
-    super.initState();
-
-    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-    ref.read(popularMoviesProvider.notifier).loadNextPage();
-    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
-    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    final initialLoading = ref.watch(initialLoadingProvider);
-
-    if (initialLoading) return const FullScreenLoader();
-
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
-    final slideShoeMovies = ref.watch(moviesSlideshowProvider);
-    final popularMovies = ref.watch(popularMoviesProvider);
-    final upcomingMovies = ref.watch(upcomingMoviesProvider);
-    final topRatedMovies = ref.watch(topRatedMoviesProvider);
-    
-
-    if (slideShoeMovies.isEmpty) return CircularProgressIndicator();
-    
-
-    return CustomScrollView(
-
-      slivers: [
-
-        const SliverAppBar(
-          floating: true,
-          flexibleSpace: FlexibleSpaceBar(
-            title: CustomAppbar(),
-          ),
-        ),
-
-
-        SliverToBoxAdapter(
-          child: Column(
-            children: [
-              // const CustomAppbar(),
-
-              MoviesSlideshow(movies: slideShoeMovies),
-
-              MovieHorizontalListview(
-                movies: nowPlayingMovies,
-                title: 'En Cines',
-                subTitle: DateFormat('EEEE d', 'es').format(DateTime.now()),
-                loadNextPage: () => ref.read(nowPlayingMoviesProvider.notifier).loadNextPage(),
-              ),
-
-              MovieHorizontalListview(
-                movies: upcomingMovies,
-                title: 'Proximamente',
-                // subTitle: "lunes 20",
-                loadNextPage: () => ref.read(upcomingMoviesProvider.notifier).loadNextPage(),
-              ),
-
-              MovieHorizontalListview(
-                movies: popularMovies,
-                title: 'Populares',
-                // subTitle: "lunes 20",
-                loadNextPage: () => ref.read(popularMoviesProvider.notifier).loadNextPage(),
-              ),
-
-              MovieHorizontalListview(
-                movies: topRatedMovies,
-                title: 'Mejor valoradas',
-                // subTitle: "lunes 20",
-                loadNextPage: () => ref.read(topRatedMoviesProvider.notifier).loadNextPage(),
-              ),
-
-              const SizedBox(height: 20),
-          
-          
-            ],
-          ),
-        ),
-      ],
-    );
-  }
-}

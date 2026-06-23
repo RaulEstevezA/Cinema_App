f0a2165b49a5162b8d12bf610ac92d66b83aec4d
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-06-23 14:32:44 +0200

    refactor: agregar navegación personalizada al BottomNavigationBar utilizando StatefulNavigationShell

--- lib/presentation/widgets/share/custom_bottom_navigation.dart
+++ lib/presentation/widgets/share/custom_bottom_navigation.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigation extends StatelessWidget {
-  const CustomBottomNavigation({super.key});
+  final StatefulNavigationShell navigationShell;
+  
+  const CustomBottomNavigation({super.key, required this.navigationShell});
+  
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       elevation: 0,
+      currentIndex: navigationShell.currentIndex,
+      onTap: (index) => navigationShell.goBranch(index),
       items: const [
       BottomNavigationBarItem(
         icon: Icon(Icons.home_max),

eac9f038ae178040250bd0d3f0321a5993a9cab6
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-06-23 14:13:24 +0200

    refactor: actualizar la configuración de rutas utilizando StatefulShellRoute para HomeScreen y agregar FavoritesView

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,28 +1,72 @@
 import 'package:cinema_app/presentation/screens/screens.dart';
 import 'package:go_router/go_router.dart';
+import 'package:cinema_app/presentation/views/views.dart';
 
 final appRouter = GoRouter(
   
   initialLocation: '/',
   routes: [
 
-    GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => HomeScreen(),
-
-      routes: [
-        GoRoute(
-          path: '/movie/:id',
-          name: MovieScreen.name,
-          builder: (context, state) {
-            final movieId = state.pathParameters['id'] ?? 'no-id';
-            return MovieScreen(movieId : movieId);
-          },
+    StatefulShellRoute.indexedStack(
+      builder: (context, state, navigationShell) {
+        return HomeScreen(navigationShell: navigationShell);
+      },
+      branches: [
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: '/',
+              builder: (context, state) => HomeView(),
+              routes: [
+                GoRoute(
+                  path: 'movie/:id',
+                  builder: (context, state) {
+                    final movieId = state.pathParameters['id'] ?? 'no-id';
+                    return MovieScreen(movieId : movieId);
+                  },
+                ),
+              ],
+            ),
+          ],
         ),
 
+        // StatefulShellBranch(
+        //   routes: [
+        //     GoRoute(
+        //       path: '/categories',
+        //       builder: ...
+        //     ),
+        //   ],
+        // ),
 
-      ]
-    ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: '/favorites',
+              builder: (context, state) => FavoritesView(),
+            ),
+          ],
+        ),
+      ],
+    )
+
+    // GoRoute(
+    //   path: '/',
+    //   name: HomeScreen.name,
+    //   builder: (context, state) => HomeScreen(childView: HomeView(),),
+
+    //   routes: [
+    //     GoRoute(
+    //       path: '/movie/:id',
+    //       name: MovieScreen.name,
+    //       builder: (context, state) {
+    //         final movieId = state.pathParameters['id'] ?? 'no-id';
+    //         return MovieScreen(movieId : movieId);
+    //       },
+    //     ),
+
+
+    //   ]
+    // ),
   ]
 );

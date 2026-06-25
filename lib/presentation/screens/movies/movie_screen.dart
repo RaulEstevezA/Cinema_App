ed5b0d1889ba22b8777694f5ba4f70e79450d485
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-06-25 13:16:32 +0200

    refactor: agregar clase _CustomGradient para manejar gradientes personalizados

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -166,6 +166,37 @@ class _CustomSliverAppBar extends StatelessWidget {
   }
 }
 
+class _CustomGradient extends StatelessWidget {
+
+  final Alignment begin;
+  final Alignment end;
+  final List<double> stops;
+  final List<Color> colors;
+
+  const _CustomGradient({
+    required this.begin, 
+    required this.end, 
+    required this.stops, 
+    required this.colors
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: begin,
+            end: end,
+            stops: stops,
+            colors: colors,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class _MovieDetails extends StatelessWidget {
   final Movie movie;
   const _MovieDetails({required this.movie});

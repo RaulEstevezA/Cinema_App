0f732f7ba0962cd1330466c406468367a53e8dd5
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-06-25 19:13:14 +0200

    feat: agregar importación de favorite_database y limpiar código comentado en main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cinema_app/config/database/favorite_database.dart';
 import 'package:cinema_app/config/router/app_router.dart';
 import 'package:cinema_app/config/theme/app_theme.dart';
 import 'package:flutter/material.dart';
@@ -6,6 +7,28 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+
+  // await db
+  //     .into(db.favoriteMovies)
+  //     .insert(
+  //       FavoriteMoviesCompanion.insert(
+  //         movieId: 1, 
+  //         backdropPatch: 'backdropPatch.png', 
+  //         originalTitle: 'My firsth movie', 
+  //         posterPatch: 'posterpath.png', 
+  //         tile: 'Mi primera pelicula')
+  //     );
+
+  // final deleteQuery = db.delete(db.favoriteMovies);
+
+  // await deleteQuery.go();
+  
+  // final moviesQuery = await db.select(db.favoriteMovies).get();
+  // print('movies $moviesQuery');
+
   await dotenv.load(fileName: '.env');
   await initializeDateFormatting('es');
   runApp(const ProviderScope(child:  MainApp()));

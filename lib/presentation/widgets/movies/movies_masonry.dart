332218608bb6c657e09018752afad6f6abfbdf21
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-06-27 14:38:22 +0200

    feat: agregar widget MoviesMasonry para mostrar películas en un diseño de mampostería

---
+++ lib/presentation/widgets/movies/movies_masonry.dart
@@ -0,0 +1,30 @@
+import 'package:cinema_app/domain/entities/movies.dart';
+import 'package:flutter/material.dart';
+
+class MoviesMasonry extends StatefulWidget {
+
+  final List<Movie> movies;
+  final Future<List<Movie>> Function()? loadNextPage;
+
+  const MoviesMasonry({super.key, required this.movies, this.loadNextPage});
+
+  @override
+  State<MoviesMasonry> createState() => _MoviesMasonryState();
+}
+
+class _MoviesMasonryState extends State<MoviesMasonry> {
+
+  bool siLastPage = false;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+

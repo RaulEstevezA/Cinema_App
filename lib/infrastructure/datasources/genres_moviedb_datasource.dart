00724e92414b7ea6f66b0db6d6a85955c48045e1
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-07-01 14:09:18 +0200

    feat: agregar clase AllGenresDatasource para obtener géneros desde la API de The Movie Database

---
+++ lib/infrastructure/datasources/genres_moviedb_datasource.dart
@@ -0,0 +1,31 @@
+import 'package:cinema_app/config/constants/enviroment.dart';
+import 'package:cinema_app/domain/datasources/genres_datasource.dart';
+import 'package:cinema_app/domain/entities/genres.dart';
+import 'package:cinema_app/infrastructure/mappers/genre_mappers.dart';
+import 'package:cinema_app/infrastructure/models/moviedb/genre_moviedb.dart';
+import 'package:dio/dio.dart';
+
+class AllGenresDatasource extends GenresDatasource{
+  
+  final dio = Dio(BaseOptions(
+    baseUrl: 'https://api.themoviedb.org/3',
+    queryParameters: {
+      'api_key': Enviroment.theMovideDbKey,
+      'language': 'es-ES',
+    },
+  ));
+  
+  
+  @override
+  Future<List<Genres>> getGenre(String allGenre) async {
+    final response = await dio.get(
+      'genre/movie/list',
+    );
+
+    final castResponse = GenreMovies.fromJson(response.data);
+
+    List<Genres> genres= castResponse.genres.map((genres) => GenreMapper.genresDBToEntity(genres)).toList();
+
+    return genres;
+  }
+}

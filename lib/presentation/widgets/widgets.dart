32f7a2a963cf16acc68ef097f708369e6b9139e6
Raul Estevez <121643646+RaulEstevezA@users.noreply.github.com>
2026-07-01 19:33:05 +0200

    feat: export MovieVerticalListview en el archivo widgets.dart

--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -4,3 +4,4 @@ export 'movies/movies_slideshow.dart';
 export 'share/custom_appbar.dart';
 export 'share/custom_bottom_navigation.dart';
 export 'movies/movie_horizontal_listview.dart';
+export 'movies/movie_vertical_listview.dart';
